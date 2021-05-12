a259e64d544b6135c067dda701568f92008aa4cb
Francisco Sanchez Siale <franky.ssiale@gmail.com>
2021-05-12 13:10:55 +0200

    first Commit

---
+++ spring-boot-data-jpa-jquery-checkinSystem/src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaJqueryCheckinSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaJqueryCheckinSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaJqueryCheckinSystemApplication.class, args);
+	}
+
+}
